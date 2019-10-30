f6e2f2cebc3340eb4845f9e2d55d22c68c0ff47c
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-30 20:31:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,9 +5,9 @@ class Student
   attr_accessor :id, :name, :grade
 
   def initialize(id=nil, name, grade)
+    @id = id
     @name = name
     @grade = grade
-    @id = id
   end
 
   def self.create_table
